8b2dae96a1971adbdb67073021509cb119a3a03e
mmchost <41358529+mmchost@users.noreply.github.com>
2019-03-26 11:06:23 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+function GetParameterByName(name, url)
+{
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
 function RefreshCartItemsCount(list)
 {
 	var cartItemsCount = document.getElementById("cartitemscount");
