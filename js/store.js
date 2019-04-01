a47277eb215f87b3612e192e1b16cabdd8294b30
mmchost <41358529+mmchost@users.noreply.github.com>
2019-04-01 16:53:48 +0300

    Update store.js

--- js/store.js
+++ js/store.js
@@ -143,7 +143,7 @@ function InitializeQt()
 		{
 			qtCell.innerHTML =
 				"<table style='display:inline-block;' cellpadding='0' cellspacing='0'><tr>" +
-				"<td><button title='Cantitate' class='button-numeric' onclick='CalculateTotal(" + i + "," + -1 + ")'>-</button></td>" + 
+				"<td><button title='Cantitate' class='button-numeric' onclick='CalculateTotal(" + i + "," + -1 + ")'>–</button></td>" + 
 				"<td style='border-style:solid; border-color:#ccc; border-width:1px 0px 1px 0px;'><input class='textbox-numeric' type='text' value='" + products[i]["quantity"] + "' style='width:40px;' disabled='true' title='Cantitate'/></td>" + 
 				"<td><button title='Cantitate' class='button-numeric' onclick='CalculateTotal(" + i + "," + 1 + ")'>+</button></td>" +
 				"</tr></table>"
