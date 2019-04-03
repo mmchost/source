1a69f762df4aa4528b86e58f941747fa10c8e9a4
mmchost <41358529+mmchost@users.noreply.github.com>
2019-04-03 16:49:42 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -120,6 +120,7 @@ function InitializeComponent(isStore, addCart, imagesPath, isMainPage, root)
 	var servicesMenu = "<a href='" + root + "#exams'>SERVICII</a>";
 	var cartMenu = "";
 	var appLogo = "<a href='https://www.mediosmedical.ro' style='display:block;'><img class='header-logo' src='" + imagesPath + "logo.png' alt='Medios Medical Center'/></a>";
+	var resxMenu = "<td class='menu-item'><a href='resources'>PENTRU MEDICI</a></td>";
 	
 	if (isMainPage == true)
 	{
@@ -140,6 +141,7 @@ function InitializeComponent(isStore, addCart, imagesPath, isMainPage, root)
 	}
 	if (isStore == true)
 	{
+		resxMenu = "";
 		servicesMenu = "<a href='https://www.mediosmedical.ro/#exams'>SERVICII</a>";
 		storeMenu = "<a href='" + root + "#products'>{0}</a>";
 		if (!isMainPage)
